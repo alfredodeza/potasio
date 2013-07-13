cf0fd3c358a618e8146fe7649d2fdf5440b0b5cc
Alfredo Deza <alfredodeza@gmail.com>
2013-07-13 09:37:23 -0400

    working wsgi file

--- potasio/wsgi.py
+++ potasio/wsgi.py
@@ -1,15 +1,10 @@
 import os
 from pecan.deploy import deploy
-import pecan_mount
 
 def config_file():
     _file = os.path.abspath(__file__)
     dirname = lambda x: os.path.dirname(x)
     parent_dir = dirname(dirname(_file))
-    return os.path.join(parent_dir, 'config.py')
+    return os.path.join(parent_dir, 'prod_config.py')
 
-pecan_mount.tree.mount('/', config_file())
-pecan_mount.tree.mount('/fu', config_file())
-pecan_mount.tree.mount('/admin', config_file())
-application = pecan_mount.tree
-#application = deploy(config_file())
+application = deploy(config_file())
